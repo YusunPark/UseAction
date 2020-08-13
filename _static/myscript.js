943621e5df547c9414236d625b8ebc97eee5ba01
YusunPark <solarpy@hanyang.ac.kr>
2020-08-13 19:15:40 +0900

    [추가] 메뉴 드롭다운 위치 고정

--- _static/myscript.js
+++ _static/myscript.js
@@ -1,28 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".wy-nav-content").css("max-width", "none");
 
-	$(".wy-nav-content").css("max-width","none");
+  $("header .links__icon").on("click", function () {
+    $("header .header__searchbar").hide();
+    $("header .header__links").slideToggle();
+  });
 
-	$('header .links__icon').on('click', function (){
-		$("header .header__searchbar").hide();
-		$("header .header__links").slideToggle();
-	});
+  $("header .search__icon").on("click", function () {
+    $("header .header__links").hide();
+    $("header .header__searchbar").slideToggle(400, function () {
+      if ($(this).is(":visible")) {
+        $(this).find("input[type=text]").focus();
+      }
+    });
+  });
 
-	$('header .search__icon').on('click', function (){
-		$("header .header__links").hide();
-		$("header .header__searchbar").slideToggle(400, function() {
-			if($(this).is(':visible')) {
-				$(this).find("input[type=text]").focus();
-			}
-		});
-	});
-
-	if($('.wy-menu-vertical li.current>a').length){
-		var sidebarScrollPosition = $('.wy-menu-vertical li.current>a').offset().top;
-		$('.wy-side-scroll').scrollTop(sidebarScrollPosition-120);
-	}
-	
+  if ($(".wy-menu-vertical li.current>a").length) {
+    var sidebarScrollPosition = $(".wy-menu-vertical li.current>a").offset()
+      .top;
+    $(".wy-side-scroll").scrollTop(sidebarScrollPosition - 120);
+  }
 });
 
-$(document).ready(function(){
-	$(".wy-nav-content").css("max-width","none");
-})
+// $(document).ready(function () {
+//   $(".wy-nav-content").css("max-width", "none");
+//   $(".wy-nav-content-wrap").css("background", "#fcfcfc");
+//   $(".wy-nav-side").css("background", "#5D81BB");
+//   $(".wy-side-nav-search").css("background", "#5D81BB");
+//   $(".wy-nav-top").css("background", "#5D81BB");
+//   $(".wy-nav-side").css("width", "300px");
+// });
